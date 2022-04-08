fda31e37ab8af801e88aab2b96177909951d70ff
ZahoorAhmad <ZahoorAhmad@DESKTOP-D06LO4U>
2022-04-09 01:01:41 +0500

    Add project files.

---
+++ HomePage/Form1.cs
@@ -0,0 +1,62 @@
+namespace HomePage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Computing Society!!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nI do not fear computers.\nI fear lack of them\n2. “Access to computers and the Internet \nhas become a basic need for education in\n our society.\n”3. Computers are incredibly fast, accurate,\n and stupid.Human beings are incredibly slow,\n inaccurate, and brilliant.\nTogether they are powerful beyond imagination.\n”4. “Computing is not about computers\n any more.It is about living.\n”5. “The real danger is not that computers will \nbegin to think like men, but that men will begin \nto think like computers.\n";
+            form1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Production Society!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nAs once a wise man said that\nUnless each man produces more than\n he receives,\n increases his output,\n there will be less for him and all the others.\nProduce, produce!\nThe efficiency of most workers is beyond \nthe control of the management and depends more\n than has been supposed upon \nthe willingness of men to do their best\n";
+            form1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Data Science Society!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nAs now it's a world of Data\nwe can see data evey where \n if you want to ,\n increases your knowledge about Artificial intelligence,\n than it's a fare advise to join our society .\nProduce, produce!\nThe efficiency of most workers is beyond \nthe control of the management and depends more\n than has been supposed upon \nthe willingness of men to do their best\n";
+            form1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Quran And Sunna Society!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nLife is journey from ALLAH to ALLAH\n. If you do something only to please Allah,\n you will surely get the good reward.\n None of any good work is worthless.\n No matter what how much wealth one may have,\n if his life is without Allah,\n the life is worthless\n";
+            form1.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Takhleeq Music Society!!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nMusic is life itself.” \n“Music is the soundtrack of your life.”\n“Life is one grand, \nsweet song so start the music.”\n“Music is love in search of a word.”\n“Music is the shorthand of emotion.”\n“The only truth is music.”\n“Music is the wine that fills the cup of silence.\n";
+            form1.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Visible = true;
+            form1.label3.Text = "Hello!\n Wellcome in Fast Advanture Society!!..\nWe are here to surve you and make you mind \nmore creative\nand productive.\nThere is a lot of adventures out there,\n waiting for us to live them!”\n ...“Run towards adventure and not away from it!\n”“If you think adventure is dangerous,\n try routine.\n“If thinking about it makes you excited, \nimagine actually doing it!\n”“Don't travel just to see";
+            form1.Show();
+        }
+    }
+}
